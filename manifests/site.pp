33ee00154c968256c2d8e4e92b7172be0d73b618
Federico Voges <federico@puppetlabs.com>
2014-10-27 13:42:30 +0000

    added site manifest

---
+++ manifests/site.pp
@@ -0,0 +1,15 @@
+Package {
+  allow_virtual => false,
+}
+
+# Define filebucket 'main':
+filebucket { 'main':
+  server => $::server,
+  path => false,
+}
+
+# Make filebucket 'main' the default backup location for all File resources:
+File { backup => 'main' }
+
+node default {
+}
